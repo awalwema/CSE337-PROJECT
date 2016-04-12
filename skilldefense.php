aa79a2769b4843edb8a3c04b542ffbd896ff1275
Brandon Jackson <bwjackson@oakland.edu>
2016-04-12 17:40:03 -0400

    Changes to skill and defense page - Input values from SQL

--- skilldefense.php
+++ skilldefense.php
@@ -2,6 +2,18 @@
 <?php session_start(); ?>
 <?php $user = $_COOKIE['username']; ?>
 <?php error_reporting(0); ?>
+<?php require 'dbconnect.php';
+$username = $_SESSION['username'];
+
+$query_ID = mysql_query("SELECT USER_ID FROM USER WHERE USER_SCREENNAME = '$username'");
+$row = mysql_fetch_array($query_ID);
+$usr_id= $row['USER_ID'];
+$character_id = $_GET['id'];
+$query_points = mysql_query("SELECT CHARACTER_SKILL_POINTS, CHARACTER_UNUSED_SKILL_POINTS FROM character_main WHERE CHARACTER_ID = '$character_id'");
+$row2 = mysql_fetch_array($query_points);
+$skillPoints = $row2['CHARACTER_SKILL_POINTS'];
+$unusedPoints = $row2['CHARACTER_UNUSED_SKILL_POINTS'];
+?>
 <html lang="en">
 
         <head>
@@ -25,6 +37,21 @@
         <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
         <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
     <![endif]-->
+
+            <script>
+
+                    function findTotal(){
+                        var arr = document.getElementsByName('qty');
+                        var tot= parseInt(<?php echo $skillPoints; ?>);
+                        for(var i=0;i<arr.length;i++){
+                            if(parseInt(arr[i].value))
+                                tot -= parseInt(arr[i].value);
+                        }
+                        document.getElementById('total').value = tot;
+                    }
+
+            </script>
+
 
     </head>
 
@@ -125,18 +152,21 @@
                         <h3 class="text-warning panel-heading">Skills & Defenses</a></h3>
                         <br><br>
                         <form class="form-horizontal">
+                            <h1>Skill Points:
+                            <input type="text" id="total" style="border: none" readonly value= '<?php echo $unusedPoints ?>' /> </h1>
+                            <br>
                             <h1>Skills</h1>
                             <br>
                             <div class="text">
                                 <label for="aracana"> Arcana: &nbsp</label>
-                                <input type="text" name = "arcana" placeholder="Enter Points"/>
+                                <input type="text" id = "arcana" name = "qty" placeholder="Enter Points" onblur="javascript:findTotal();" />
                                 <p>Arcana is your ability to interact with and understand Magic. It’s Analyzing and
                                 Manipulating magical effects, similar to how Dexterity can fiddle with traps or machines.</p>
                             </div>
                             <br>
                             <div class="text">
                                 <label for="dexterity"> Dexterity:   &nbsp</label>
-                                <input type="text" name = "dexterity" placeholder="Enter Points"/>
+                                <input type="text" id = "dexterity" name = "qty" placeholder="Enter Points" onblur="javascript:findTotal();"/>
                                 <p>Dexterity is your Fine Motor Skills - lock-picking, machine-jiggering and
                                 trap-disabling/resetting.  Dexterity also governs aiming in non-combat situations,
                                 like tossing a rock and hitting a lever across the room.</p>
@@ -144,40 +174,40 @@
                             <br>
                             <div class="text">
                                 <label for="heal"> Heal:   &nbsp</label>
-                                <input type="text" name = "heal" placeholder = "Enter Points"/>
+                                <input type="text" id = "heal" name = "qty" placeholder="Enter Points" onblur="javascript:findTotal();"/>
                                 <p>Heal is your ability to remove Afflictions, analyze wounds, diseases etc</p>
                             </div>
                             <br>
                             <div class="text">
                                 <label for= "insight"> Insight:    &nbsp </label>
-                                <input type= "text" name = "insight" placeholder = "Enter Points"/>
+                                <input type="text" id = "insight" name = "qty" placeholder="Enter Points" onblur="javascript:findTotal();"/>
                                 <p>Insight is like your social perception. It’s reading peoples' expressions, tones of
                                 voice and similar to determine what they're feeling and whether they're lying.</p>
                             </div>
                             <br>
                             <div class="text">
                                 <label for= "might"> Might:     &nbsp</label>
-                                <input type= "text" name = "might" placeholder = "Enter Points"/>
+                                <input type="text" id = "might" name = "qty" placeholder="Enter Points" onblur="javascript:findTotal();"/>
                                 <p>Might is your physical strength - swimming against the current, leaping wide chasms,
                                 breaking down doors</p>
                             </div>
                             <br>
                             <div class="text">
                                 <label for= "perception"> Perception:    &nbsp </label>
-                                <input type= "text" name = "perception" placeholder = "Enter Points"/>
+                                <input type="text" id = "perception" name = "qty" placeholder="Enter Points" onblur="javascript:findTotal();"/>
                                 <p>Perception is your ability to notice things with your physical senses (all of them –
                                 sight, hearing, touch, smell, etc.)</p>
                             </div>
                             <br>
                             <div class="text">
                                 <label for= "Stealth"> Stealth:   &nbsp  </label>
-                                <input type= "text" name = "stealth" placeholder = "Enter Points"/>
+                                <input type="text" id = "stealth" name = "qty" placeholder="Enter Points" onblur="javascript:findTotal();"/>
                                 <p>Stealth is your ability to move about discretely - to successfully stay quiet and avoid being noticed. </p>
                             </div>
                             <br>
                             <div class="text">
                                 <label for= "generalknowledge"> General Knowledge:   &nbsp   </label>
-                                <input type= "text" name = "generalknowledge" placeholder = "Enter Points"/>
+                                <input type="text" id = "generalknowledge" name = "qty" placeholder="Enter Points" onblur="javascript:findTotal();"/>
                                 <p>Your knowledge of things that don’t fall into the other skills. Who's the king of
                                 this land? What’s the historical significance of the strange coin you just found?
                                 How do you cook a decent meal? Which mushrooms are safe to eat?</p>
@@ -186,18 +216,18 @@
                             <h1> Defenses </h1>
                             <br>
                             <div class="text">
-                                <la-bel for= "endurance"> Endurance:   &nbsp   </label>
-                                <input type= "text" name = "endurance" placeholder = "Enter Points"/>
+                                <label for= "endurance"> Endurance:   &nbsp   </label>
+                                <input type="text" id = "endurance" name = "qty" placeholder="Enter Points" onblur="javascript:findTotal();"/>
                                 <p>Enduring serious damage, wounds and diseases through sheer physical toughness</p>
                             </div>
                             <div class="text">
                                 <label for= "reflexes"> Reflexes:   &nbsp   </label>
-                                <input type= "text" name = "reflexes" placeholder = "Enter Points"/>
+                                <input type="text" id = "reflexes" name = "qty" placeholder="Enter Points" onblur="javascript:findTotal();"/>
                                 <p>Quickly react to danger, dodging traps and responding to sucker-punches.</p>
                             </div>
                              <div class="text">
                                  <label for= "will"> Will:   &nbsp   </label>
-                                 <input type= "text" name = "will" placeholder = "Enter Points"/>
+                                 <input type="text" id = "will" name = "qty" placeholder="Enter Points" onblur="javascript:findTotal();"/>
                                  <p>Mentally resisting mind-altering effects, or similar mental assaults (from resisting
                                   body possession and charm spells to holding onto one's sanity in the face of
                                   abominations).</p>
